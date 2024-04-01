2759dc6aee1a020ba59336e8007410c7ace9143d
Marek Mojžiš <mojzis9@seznam.cz>
2024-04-01 19:27:14 +0200

    fix text

--- src/routes/about-me/+page.svelte
+++ src/routes/about-me/+page.svelte
@@ -39,26 +39,26 @@
   <h1 bind:this={title} class="text-3xl md:text-5xl text-left">Vítej!</h1>
   <div class="mt-6 space-y-4 text-gray-600">
     <p class="text-base md:text-lg leading-relaxed">
-      Moje jméno je Magdaléna. Jsem ráda, že Tě můžu přivítat v mé malé online
-      galerii.
+      Moje jméno je Magdaléna. Jsem ráda, že Tě můžu přivítat v&nbsp;mé malé
+      online galerii.
     </p>
     <p class="text-base md:text-lg leading-relaxed">
-      Malování vnímám jako projev svobody a kreativity. Neexistují zde pro mne
-      pravidla. Někdy vím co maluju a jak to má vypadat, a jindy do posledního
-      čmárnutí nevím, co bude výsledkem.
+      Malování vnímám jako projev svobody a&nbsp;kreativity. Neexistují zde pro
+      mne pravidla. Někdy vím co&nbsp;maluju a&nbsp;jak to má vypadat,
+      a&nbsp;jindy do&nbsp;posledního čmárnutí nevím, co&nbsp;bude výsledkem.
     </p>
     <p class="text-base md:text-lg leading-relaxed">
-      Moje obrázky jsou často inspirovány nějakou emocí nebo prožitkem - od
-      radosti a naděje, přes smutek a melancholii, až po tak trochu dětskou
-      zvídavost a lásku.
+      Moje obrázky jsou často inspirovány nějakou emocí nebo prožitkem -
+      od&nbsp;radosti a&nbsp;naděje, přes smutek a&nbsp;melancholii, až po tak
+      trochu dětskou zvídavost a&nbsp;lásku.
     </p>
     <p class="text-base md:text-lg leading-relaxed">
-      Moje obrázky jsou často inspirovány nějakou emocí nebo prožitkem - od
-      radosti a naděje, přes smutek a melancholii, až po tak trochu dětskou
-      zvídavost a lásku.
+      Baví mě, že si každý může v&nbsp;obrazech najít něco osobního, něco, co ho
+      oslovuje a&nbsp;rezonuje s&nbsp;jeho vlastními prožitky a&nbsp;pocity.
     </p>
     <p class="text-base md:text-lg leading-relaxed">
-      Díky za to, že to tu chceš omrknout a přeju Ti spoustu barevných dnů.
+      Díky za to, že to tu chceš omrknout a&nbsp;přeju&nbsp;Ti spoustu barevných
+      dnů.
     </p>
   </div>
 </section>
